61731132b1d7dea666f6ffbd91ae36bc29be3b5e
Jow <jow@localhost.localdomain>
2014-07-15 22:23:52 -0300

    fix #4 Downloading BEL files from URL and saving into public/belfiles folder Committer: Jonathan N. Boilesen <jonathan@boilesen.com>

--- app/controllers/belfiles_controller.rb
+++ app/controllers/belfiles_controller.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 class BelfilesController < ApplicationController
   def new
   end
@@ -14,12 +15,37 @@ class BelfilesController < ApplicationController
     ## Treating file upload (:belfile) or download from URL (:url)
     if !belfile_params[:belfile].nil?
       filename = belfile_params[:belfile].original_filename
-      directory = "public/belfiles"
-      belfile_path = directory + '/' + filename
+      directory = "public/belfiles/"
+      belfile_path = directory + filename
       if !File.exist?(belfile_path)
         @belfile.belfile_path = belfile_path
         path = File.join(directory, filename)
         File.open(path, "wb") { |f| f.write(belfile_params[:belfile].read) }
+        @belfile.save
+        redirect_to @belfile
+      else
+
+        redirect_to new_belfile_path
+      end
+    elsif !belfile_params[:url].nil?
+      @belfile = Belfile.new
+      @belfile.title = belfile_params[:title]
+      @belfile.description = belfile_params[:description]
+      ## Filename will be based on title
+      filename = @belfile.title
+      ## So, we sanitize it
+      filename.gsub!(/^.*(\\|\/)/, '')
+      filename.gsub!(/[^0-9A-Za-z.\-]/, '_')
+      directory = 'public/belfiles/'
+      filename = filename + '.txt'
+      belfile_path = directory + filename
+      ## If it not exists, create it
+      if !File.exist?(belfile_path)
+        @belfile.belfile_path = belfile_path
+        path = File.join(directory, filename)
+        open(path, 'wb') do |file|
+          file << open(belfile_params[:url]).read
+        end
         @belfile.save
         redirect_to @belfile
       else
