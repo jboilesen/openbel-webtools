bb5d9df4701870b5abcebdee03072ac223ec962c
Jonathan N. Boilesen <jonathan@boilesen.com>
2014-08-07 03:06:08 -0300

    javascript scripts added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require cytoscape.js/dist/cytoscape.js
+//= require d3.js/d3.js
+//= require scripts.js
---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,4 @@
+/*
+ * OpenBEL Webtools
+ *
+ */
